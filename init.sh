263bf54a0456676deb622000f72072f575c5ab83
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2023-09-08 16:39:30 +0545

    fix: updated init script

--- init.sh
+++ init.sh
@@ -12,11 +12,11 @@ setopt AUTO_CD
 # Enable key repeat and hold
 defaults write -g ApplePressAndHoldEnabled -bool false;
 
-mkdir $HOME/.go/
-mkdir -p $HOME/.config/nvim
-mkdir -p $HOME/.config/tmux
+mkdir "$HOME/.go/"
+mkdir -p "$HOME/.config/nvim"
+mkdir -p "$HOME/.config/tmux"
 
-ln -s .zshrc $HOME/.zshrc
-ln -s .zshenv $HOME/.zshenv
-ln -s nvim $HOME/.config/nvim
-ln -s .tmux $HOME/.config/tmux
+ln -s "$(pwd)/.zshrc" "$HOME/.zshrc"
+ln -s "$(pwd)/.zshenv" "$HOME/.zshenv"
+ln -s "$(pwd)/nvim " "$HOME/.config/nvim"
+ln -s "$(pwd)/.tmux " "$HOME/.config/tmux"
