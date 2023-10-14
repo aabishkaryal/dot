8404cdb26621a1005c7609395830df1214b162c0
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2023-10-14 16:31:28 +0545

    fix: moved to old nvim config

---
+++ nvim/lua/user/autocommands.lua
@@ -0,0 +1,67 @@
+local custom_autocmd_grp = vim.api.nvim_create_augroup("custom_autocmd",
+	{ clear = true })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+	callback = function()
+		vim.cmd [[
+      nnoremap <silent> <buffer> q :close<CR>
+      set nobuflisted
+    ]]
+	end,
+	group = custom_autocmd_grp,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+	group = custom_autocmd_grp,
+})
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+	command = "tabdo wincmd =",
+	group = custom_autocmd_grp,
+})
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	callback = function()
+		vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+	end,
+	group = custom_autocmd_grp,
+})
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+	command = "hi link illuminatedWord LspReferenceText",
+	group = custom_autocmd_grp,
+})
+
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+	callback = function()
+		local line_count = vim.api.nvim_buf_line_count(0)
+		if line_count >= 5000 then
+			vim.cmd "IlluminatePauseBuf"
+		end
+	end,
+	group = custom_autocmd_grp,
+})
+
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+	command = "lua vim.diagnostic.open_float(nil, {focus=false})",
+	group = custom_autocmd_grp,
+})
+
+local filetypes = {
+	jrnl = { "*.jrnl" },
+	gitignore = { "*.dockerignore" },
+	env = { ".env.*", ".env" },
+}
+for filetype, patterns in pairs(filetypes) do
+	vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+		group = custom_autocmd_grp,
+		command = "setfiletype " .. filetype,
+		pattern = patterns,
+	})
+end
---
+++ nvim/lua/user/lsp/settings/lua_ls.lua
@@ -0,0 +1,20 @@
+return {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = {
+                    'vim'
+                }
+            },
+            workspace = {
+                library = {
+                    [vim.fn.expand '$VIMRUNTIME/lua'] = true,
+                    [vim.fn.stdpath 'config' .. '/lua'] = true
+                }
+            },
+            telemetry = {
+                enable = false
+            }
+        }
+    }
+}
