616c18cc03e565e87c973ad16a249e956ccc8121
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2024-04-23 08:17:43 +0545

    fix: basic keymaps, options and plugins installed

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system {
-		'git',
-		'clone',
-		'--depth',
-		'1',
-		'https://github.com/wbthomason/packer.nvim',
-		install_path
-	}
-	print 'Installing packer close and reopen Neovim...'
-	vim.cmd [[packadd packer.nvim]]
+  PACKER_BOOTSTRAP = fn.system {
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  }
+  print 'Installing packer close and reopen Neovim...'
+  vim.cmd [[packadd packer.nvim]]
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -26,43 +26,54 @@ vim.cmd [[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, 'packer')
 if not status_ok then
-	return
+  return
 end
 
 -- Have packer use a popup window
 packer.init {
-	display = {
-		open_fn = function()
-			return require('packer.util').float {
-				border = 'rounded'
-			}
-		end
-	},
-	git = {
-		clone_timeout = 300 -- Timeout, in seconds, for git clones
-	}
+  display = {
+    open_fn = function()
+      return require('packer.util').float {
+        border = 'rounded'
+      }
+    end
+  },
+  git = {
+    clone_timeout = 300 -- Timeout, in seconds, for git clones
+  }
 }
 
 -- Install your plugins here
 return packer.startup(function(use)
-	use { 'wbthomason/packer.nvim' } -- Have packer manage itself
-	use { 'nvim-lua/plenary.nvim' } -- Useful lua functions used by lots of plugins
+  use {
+    'wbthomason/packer.nvim'
+  } -- Have packer manage itself
+  use {
+    'nvim-lua/plenary.nvim'
+  } -- Useful lua functions used by lots of plugins
 
-  	-- Colorschemes
-	use { 'catppuccin/nvim', as = 'catppuccin' }
-	use { 'Mofiqul/dracula.nvim' }
-  
-  	-- Treesitter
-	use { 'nvim-treesitter/nvim-treesitter' }
-	use({
-		'nvim-treesitter/nvim-treesitter-textobjects',
-		after = 'nvim-treesitter',
-		requires = 'nvim-treesitter/nvim-treesitter'
-	})
+  -- Colorschemes
+  use {
+    'catppuccin/nvim',
+    as = 'catppuccin'
+  }
+  use {
+    'Mofiqul/dracula.nvim'
+  }
+
+  -- Treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter'
+  }
+  use({
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    after = 'nvim-treesitter',
+    requires = 'nvim-treesitter/nvim-treesitter'
+  })
 
   -- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if PACKER_BOOTSTRAP then
-		require('packer').sync()
-	end
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require('packer').sync()
+  end
 end)
