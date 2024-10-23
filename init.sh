82cca5b1ef6299cc6451466bdd192954a7e62053
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2024-10-23 18:40:12 +0545

    feat: add configuration for ytdlp

--- init.sh
+++ init.sh
@@ -19,7 +19,6 @@ nvm alias default 20
 # Install Tmux package manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-
 # Enable key repeat and hold
 defaults write -g ApplePressAndHoldEnabled -bool false;
 
@@ -28,17 +27,19 @@ mkdir "$HOME/.go/"
 mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/tmux"
 mkdir -p "$HOME/repos/aabishkaryal"
+mkdir -p "$HOME/.config/yt-dlp"
 git clone https://github.com/aabishkaryal/dot.git
 
 # Link config files
 ln -s "$(pwd)/.functions" "$HOME/.functions"
 ln -s "$(pwd)/.zshrc" "$HOME/.zshrc"
 ln -s "$(pwd)/.zshenv" "$HOME/.zshenv"
-ln -s "$(pwd)/nvim" "$HOME/.config/nvim"
-ln -s "$(pwd)/kitty" "$HOME/.config/kitty"
-ln -s "$(pwd)/tmux" "$HOME/.config/tmux"
+ln -s "$(pwd)/nvim" "$HOME/.config/"
+ln -s "$(pwd)/kitty" "$HOME/.config/"
+ln -s "$(pwd)/tmux" "$HOME/.config/"
 ln -s "$(pwd)/.ssh/config" "$HOME/.ssh/config"
 ln -s "$(pwd)/.p10k.zsh" "$HOME/.p10k.zsh"
 ln -s "$(pwd)/scripts" "$HOME/.scripts"
+ln -s "$(pwd)/yt-dlp" "$HOME/.config/"
 
 git config --global url."ssh://git@github.com/".insteadOf "https://github.com/"
